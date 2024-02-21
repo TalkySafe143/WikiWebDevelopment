a0ca8d1e889ad1dd9783e37ee02f62dde32566fc
Sebastian Galindo <sebastian.galindo143@gmail.com>
2024-02-20 19:34:34 -0500

    Adding controller Request

--- src/main/java/com/wiki/main/controllers/HomeController.java
+++ src/main/java/com/wiki/main/controllers/HomeController.java
@@ -3,8 +3,10 @@ package com.wiki.main.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/grupo-2-1/galindo")
 public class HomeController {
 
     @GetMapping("/")
